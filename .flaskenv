c2230e07df291136bb5c2727708fec41b46cbb3c
Ben Watson <52138939+figchutney@users.noreply.github.com>
2021-03-26 12:03:02 +0000

    Simplify way app is run (#9)
    * Simplify way app is run
    
    * Update tests to conform to new app structure

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,2 @@
-FLASK_APP=fagiolo.fagiolo.py
+FLASK_APP=fagiolo.app:app
 FLASK_ENV=development
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,15 @@
 import pytest
 from flask.testing import FlaskClient
 
-from fagiolo import fagiolo
+from fagiolo.app import app
 
 
 @pytest.fixture
 def client() -> FlaskClient:
-    fagiolo.app.config["DEBUG"] = True
-    fagiolo.app.config["TESTING"] = True
-    fagiolo.app.config["PROPAGATE_EXCEPTIONS"] = False
-    fagiolo.app.config["PRESERVE_CONTEXT_ON_EXCEPTION"] = False
+    app.config["DEBUG"] = True
+    app.config["TESTING"] = True
+    app.config["PROPAGATE_EXCEPTIONS"] = False
+    app.config["PRESERVE_CONTEXT_ON_EXCEPTION"] = False
 
-    with fagiolo.app.test_client() as client:
+    with app.test_client() as client:
         yield client
